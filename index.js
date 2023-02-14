429dd430446ea90df897de918320b706779487cc
Adriana <adriana.s.pico.c@gmail.com>
2023-02-13 22:03:30 -0500

    Database

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const mongoose =require("mongoose");
 
-const app = express( );
 const PORT = 3030;
-
+const app = express( );
 
 const todoRoutes = require("./routes/todoRoutes");
  
+mongoose.connect("mongodb+srv://adrianapico:1096244597@todocambiotodolist.mc9qxv9.mongodb.net/TodoCambioTodoList")
+    .then(() => console.log ("Connected successfully"))
+    .catch ((err) => console.error(err));
 app.use("/todos", todoRoutes);
 
 app.listen(PORT, ( )=> {
